8578db5452890883868029aa7285114b11fe6112
Muhammad Adeel Chaudhary <madeelchaudhry@gmail.com>
2021-04-26 17:44:06 +0500

    Some testing

---
+++ src/list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function list() {
+    return (
+            <ol>
+            <li>Item-1</li>
+            <li>Item-2</li>
+            <li>Item-3</li>
+            <li>Item-4</li>
+            <li>Item-5</li>
+            </ol>
+    )
+}
